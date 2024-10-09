74b37b0bce10c076b58449afcfbc568ba5c7524d
chead <chead@chead.net>
2024-10-09 06:53:30 -0700

    Removed extraneous comments

--- Poast/View Models/PoastProfileViewModel.swift
+++ Poast/View Models/PoastProfileViewModel.swift
@@ -64,37 +64,3 @@ enum PoastProfileViewModelError: Error {
         return .unknown
     }
 }
-
-//struct PoastProfileViewPreviewModel: PostProfileViewModeling {
-//    let handle: String
-//
-//    func getProfile(session: PoastSessionObject) async -> Result<PoastProfileModel?, PoastProfileViewModelError> {
-//        let managedObjectContext = PersistenceController.preview.container.viewContext
-//
-//        let account = PoastAccountObject(context: managedObjectContext)
-//
-//        account.created = Date()
-//        account.handle = "Foobar"
-//        account.host = URL(string: "https://bsky.social")!
-//        account.uuid = UUID()
-//
-//        let session = PoastSessionObject(context: managedObjectContext)
-//
-//        session.created = Date()
-//        session.accountUUID = account.uuid!
-//        session.did = ""
-//
-//        let profile = PoastProfileModel(did: "0",
-//                                        handle: "foobar",
-//                                        displayName: "FOOBAR",
-//                                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a tortor dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a felis sit amet elit viverra porttitor. In hac habitasse platea dictumst. Nulla mollis luctus sagittis. Vestibulum volutpat ipsum vel elit accumsan dapibus. Vivamus quis erat consequat, auctor est id, malesuada sem.",
-//                                        avatar: "",
-//                                        banner: "",
-//                                        followsCount: 0,
-//                                        followersCount: 1,
-//                                        postsCount: 2,
-//                                        labels: [])
-//
-//        return .success(profile)
-//    }
-//}
